4007abc3f7c5e7b59e8ce248ee49d71e4d9537f6
noa.despaux <noa.despaux@etu.umontpellier.fr>
2025-03-11 17:56:29 +0100

    format

--- src/test/kotlin/fr/umontpellier/springcity/ApiTests.kt
+++ src/test/kotlin/fr/umontpellier/springcity/ApiTests.kt
@@ -14,45 +14,55 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
 @WebMvcTest
-class ApiTests(@Autowired val mockMvc: MockMvc) {
-
+class ApiTests(
+    @Autowired val mockMvc: MockMvc,
+) {
     @MockkBean
     private lateinit var cityRepository: CityRepository
 
     @Test
     fun testHealthCheck() {
-        mockMvc.perform(get("/_health"))
-            .andExpect(status().`is`(204));
+        mockMvc
+            .perform(get("/_health"))
+            .andExpect(status().`is`(204))
     }
 
     @Test
     fun testGetAllCities() {
-        every { cityRepository.findAll() } returns listOf(
-            City(1, "31", "feur", "31790", "Saint-Sauveur", 45.610769, 2.876716),
-        )
-        mockMvc.perform(get("/city"))
+        every { cityRepository.findAll() } returns
+            listOf(
+                City(1, "31", "feur", "31790", "Saint-Sauveur", 45.610769, 2.876716),
+            )
+        mockMvc
+            .perform(get("/city"))
             .andExpect(status().`is`(200))
             .andExpect(content().contentType(MediaType.APPLICATION_JSON))
     }
 
     @Test
     fun testPostCity() {
-        every { cityRepository.save(any()) } returns City(id = 1, departmentCode = "31", inseeCode = "feur", zipCode = "31790", name = "Saint-Sauveur", lat = 45.610769, lon = 2.876716)
+        every {
+            cityRepository.save(any())
+        } returns
+            City(id = 1, departmentCode = "31", inseeCode = "feur", zipCode = "31790", name = "Saint-Sauveur", lat = 45.610769, lon = 2.876716)
 
-        mockMvc.perform(post("/city")
-            .contentType(MediaType.APPLICATION_JSON)
-            .content("""
-                {
-                    "department_code": "31",
-                    "insee_code": "feur",
-                    "zip_code": "31790",
-                    "name": "Saint-Sauveur",
-                    "lat": 45.610769,
-                    "lon": 2.876716
-                }
-            """.trimIndent()))
-            .andExpect(status().`is`(201))
+        mockMvc
+            .perform(
+                post("/city")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(
+                        """
+                        {
+                            "department_code": "31",
+                            "insee_code": "feur",
+                            "zip_code": "31790",
+                            "name": "Saint-Sauveur",
+                            "lat": 45.610769,
+                            "lon": 2.876716
+                        }
+                        """.trimIndent(),
+                    ),
+            ).andExpect(status().`is`(201))
             .andExpect(content().contentType(MediaType.APPLICATION_JSON))
     }
-
 }
